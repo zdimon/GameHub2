e01ddec710ee36e4dcda6e714a10944e843c56a3
zdimon <zdimon77@gmail.com>
2018-01-25 19:28:05 +0300

    auto commit

--- dj/main/views.py
+++ dj/main/views.py
@@ -1,3 +1,9 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+
+# Create your views here.
 from django.shortcuts import render
 
 # Create your views here.
